b29652f0684e3142711e4c5cc1e074fbe611e3b2
carlalacroix <90257420+carlalacroix@users.noreply.github.com>
2022-09-04 22:51:15 +0200

    Update slideshow => Add Projets / Brico

--- js/main.js
+++ js/main.js
@@ -1,92 +1,145 @@
 $("#slideshow > figure:gt(0)").hide();
 
-setInterval(function() { 
-  $('#slideshow > figure:first')
-  .fadeOut(1000)
-  .next()
-  .fadeIn(1000)
-  .end()
-  .appendTo('#slideshow');
-}, 1000);
+var aboutDiapoLength = $('#slideshow > figure').length;
+var currentDiapo = 0;
+setTimeout(function (){
+  setInterval(function() {
+    $('#slideshow > figure:first')
+        .fadeOut(800)
+        .next()
+        .fadeIn(800)
+        .end()
+        .appendTo('#slideshow');
+  }, 1600);
+}, 2600)
 
-$(".slideshowInspi1 > figure:gt(0)").hide();
+$("#inspiration .slideshowInspi1 > figure:gt(0)").hide();
 
 setTimeout(function(){
   setInterval(function() { 
-    $('.slideshowInspi1 > figure:first')
+    $('#inspiration .slideshowInspi1 > figure:first')
     .fadeOut(1500)
     .next()
     .fadeIn(1500)
     .end()
-    .appendTo('.slideshowInspi1');
-  }, 2500);
+    .appendTo('#inspiration .slideshowInspi1');
+  }, 4000);
 }, 1000)
 
 
-$(".slideshowInspi2 > figure:gt(0)").hide();
+$("#inspiration .slideshowInspi2 > figure:gt(0)").hide();
 
 setTimeout(function(){
   setInterval(function() { 
-    $('.slideshowInspi2 > figure:first')
+    $('#inspiration .slideshowInspi2 > figure:first')
     .fadeOut(1500)
     .next()
     .fadeIn(1500)
     .end()
-    .appendTo('.slideshowInspi2');
-  }, 2500);
+    .appendTo('#inspiration .slideshowInspi2');
+  }, 4000);
 }, 1500)
 
 
-$(".slideshowInspi3 > figure:gt(0)").hide();
+$("#inspiration .slideshowInspi3 > figure:gt(0)").hide();
 
 setTimeout(function(){
   setInterval(function() { 
-    $('.slideshowInspi3 > figure:first')
+    $('#inspiration .slideshowInspi3 > figure:first')
     .fadeOut(1500)
     .next()
     .fadeIn(1500)
     .end()
-    .appendTo('.slideshowInspi3');
-  }, 2500);
+    .appendTo('#inspiration .slideshowInspi3');
+  }, 4000);
 }, 2000)
 
-$(".slideshowInspi4 > figure:gt(0)").hide();
+$("#inspiration .slideshowInspi4 > figure:gt(0)").hide();
 
 setTimeout(function(){
   setInterval(function() { 
-    $('.slideshowInspi4 > figure:first')
+    $('#inspiration .slideshowInspi4 > figure:first')
     .fadeOut(1500)
     .next()
     .fadeIn(1500)
     .end()
-    .appendTo('.slideshowInspi4');
-  }, 2500);
+    .appendTo('#inspiration .slideshowInspi4');
+  }, 4000);
 }, 2100)  
 
-$(".slideshowInspi5 > figure:gt(0)").hide();
+$("#inspiration .slideshowInspi5 > figure:gt(0)").hide();
 
 setTimeout(function(){
   setInterval(function() { 
-    $('.slideshowInspi5 > figure:first')
+    $('#inspiration .slideshowInspi5 > figure:first')
     .fadeOut(1500)
     .next()
     .fadeIn(1500)
     .end()
-    .appendTo('.slideshowInspi5');
-  }, 2500);
+    .appendTo('#inspiration .slideshowInspi5');
+  }, 4000);
 }, 1600)  
 
-$(".slideshowInspi6 > figure:gt(0)").hide();
+$("#inspiration .slideshowInspi6 > figure:gt(0)").hide();
 
 setTimeout(function(){
   setInterval(function() { 
-    $('.slideshowInspi6 > figure:first')
+    $('#inspiration .slideshowInspi6 > figure:first')
     .fadeOut(1500)
     .next()
     .fadeIn(1500)
     .end()
-    .appendTo('.slideshowInspi6');
-  }, 2500);
+    .appendTo('#inspiration .slideshowInspi6');
+  }, 4000);
 }, 1100)  
 
-  
+
+
+// DIAPO PROJETS
+
+// Animate Projets images
+function diapoTravaux(classeSection,  index) {
+  var timeout;
+
+    $('' + classeSection + ' .slideshowTravaux' + index + ' > figure:gt(0)').hide();
+
+    setTimeout(function(){
+      setInterval(function() {
+        $(''+ classeSection +' .slideshowTravaux' + index + ' > figure:first')
+            .fadeOut(1500)
+            .next()
+            .fadeIn(1500)
+            .end()
+            .appendTo(''+ classeSection + ' .slideshowTravaux' + index + '');
+      }, 4000);
+    }, 1000 + 500*(index%2));
+  }
+
+function diapoDiy(classeSection,  index) {
+
+  $('' + classeSection + ' .slideshowDiy' + index + ' > figure:gt(0)').hide();
+
+  setTimeout(function(){
+    setInterval(function() {
+      $(''+ classeSection +' .slideshowDiy' + index + ' > figure:first')
+          .fadeOut(1000)
+          .next()
+          .fadeIn(1000)
+          .end()
+          .appendTo(''+ classeSection + ' .slideshowDiy' + index + '');
+    }, 2600);
+  }, 1000);
+}
+
+// Call to diapoTravaux function for each kmage block;
+for (index = 1; index < document.querySelectorAll('.travaux.convention div').length; index++) {
+  diapoTravaux('.travaux.convention', index);
+}
+
+for (index = 1; index < document.querySelectorAll('.travaux.saint-charles div').length; index++) {
+  diapoTravaux('.travaux.saint-charles', index );
+}
+
+for (index = 1; index < document.querySelectorAll('.diy div').length; index++) {
+  diapoDiy('.diy', index );
+}
